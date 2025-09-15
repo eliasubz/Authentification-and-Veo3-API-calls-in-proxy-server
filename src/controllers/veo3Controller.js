d124dda8b5ed797b1ed2d88dd8fbee1bb52ebf8e
elias <eliasls2002@yahoo.de>
2025-09-15 21:15:09 +0200

    The current veo3 controller assumes that the user is being sent over the request which is smart but not implemented (it would just be a manual postman change) but how I will handle this I have to decide later. As of now I guess all of that will be handled with a the webtokens that need to be signed in the beginning of the session. The debugging of the controler service & routes files for the veo3 service will have to wait until tomorrow

--- src/controllers/veo3Controller.js
+++ src/controllers/veo3Controller.js
@@ -4,7 +4,9 @@ const User = require('../models/User');
 
 exports.generate = async (req, res) => {
     try {
-        const userId = req.user.id;
+        console.log("Before asking for id")
+        const userId = req.user._id;
+        console.log("After asking for id", userId)
 
         // Check user credits
         const user = await User.findById(userId);
